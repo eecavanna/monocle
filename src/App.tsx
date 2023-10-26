837e8f790f54e87339f97e4eec7ae05bd03e6d4a
eecavanna <eecavanna@users.noreply.github.com>
2023-10-26 09:56:00 -0700

    Make editor controlled and update diagram from editor content

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import viteLogo from '/vite.svg'
 import CodeMirror from '@uiw/react-codemirror'
 import {Mermaid} from 'mdx-mermaid/lib/Mermaid'
@@ -7,26 +7,45 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [chart, setChart] = useState<string>("flowchart LR\na --> b")
+    const [editorValue, setEditorValue] = useState<string>("target: dep1 dep2\ndep1: dep3\n");
+    const onEditorChange = (s: string) => {
+        setEditorValue(s);
+        setIsChartStale(true);
+    };
 
-  return (
-    <>
-      <div className={"p-2 bg-info-subtle"}>
-        <Button onClick={() => alert("You clicked it")}>Click it</Button>
-      </div>
-      <div style={{marginBottom: 24}}>
-        <CodeMirror theme={"dark"}/>
-      </div>
-      <div style={{marginTop: 8, marginBottom: 24, border: "1px solid salmon"}}>
-        <Mermaid chart={generateChartFromMakefile("target: dep1 dep2\ndep1: dep2\n")} />
-      </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-    </>
-  )
+    const [isChartStale, setIsChartStale] = useState<boolean>(false);
+    const onClickMakeChart = () => {
+        const chartCode = generateChartFromMakefile(editorValue);
+        setChart(chartCode);
+        setIsChartStale(false);
+    };
+
+    const [chart, setChart] = useState<string>(generateChartFromMakefile(editorValue));
+
+    return (
+        <>
+            <div>
+                <CodeMirror theme={"dark"} value={editorValue} onChange={onEditorChange}/>
+            </div>
+            <div className={"my-2"}>
+                <Button
+                    onClick={onClickMakeChart}
+                    disabled={!isChartStale}
+                    className={isChartStale ? "shadow-sm" : "shadow-none"}
+                >
+                    Update diagram
+                </Button>
+            </div>
+            <div style={{filter: isChartStale ? "blur(4px)" : undefined}}>
+                <Mermaid chart={chart + "\n"} config={{theme: "dark"}}/>
+            </div>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+            </div>
+        </>
+    )
 }
 
 export default App
