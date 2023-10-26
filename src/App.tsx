a48b471f647c4e26b6303fb1db5b7e0c3aaa9757
eecavanna <eecavanna@users.noreply.github.com>
2023-10-26 08:15:50 -0700

    Delete React logo and additional boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import CodeMirror from '@uiw/react-codemirror'
 import {Mermaid} from 'mdx-mermaid/lib/Mermaid'
@@ -8,7 +7,6 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
   const [chart, setChart] = useState<string>("flowchart LR\na --> b")
 
   return (
@@ -26,22 +24,7 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
