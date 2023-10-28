1ca5419d7fc744be500bf4717bb860b669dfb4a2
eecavanna <eecavanna@users.noreply.github.com>
2023-10-27 21:00:45 -0700

    Add button to copy Mermaid code to clipboard

--- src/components/Diagram.tsx
+++ src/components/Diagram.tsx
@@ -1,9 +1,9 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import { useRef } from "react";
 import { Theme } from "../constants.ts";
 import { Mermaid } from "mdx-mermaid/lib/Mermaid";
 import Button from "react-bootstrap/Button";
 import { saveAs } from "file-saver";
-import { useRef } from "react";
+import copy from "copy-to-clipboard";
 
 interface Props {
   theme?: Theme;
@@ -16,7 +16,7 @@ const MIME_TYPE_SVG = "image/svg+xml";
 const Diagram = ({ theme = Theme.Light, isStale, mermaidCode }: Props) => {
   const diagramWrapperRef = useRef<HTMLDivElement>(null);
 
-  const onClickSave = () => {
+  const onClickDownloadSvg = () => {
     if (diagramWrapperRef.current !== null) {
       const svgEls = diagramWrapperRef.current.getElementsByTagName("svg");
       const svgEl = svgEls[0]; // get the first one
@@ -29,12 +29,18 @@ const Diagram = ({ theme = Theme.Light, isStale, mermaidCode }: Props) => {
     }
   };
 
+  const onClickCopyMermaidCode = () => {
+    const onCopy = () => {
+      alert("Copied Mermaid diagram code to clipboard.");
+    };
+    copy(mermaidCode, { onCopy });
+  };
+
   const backgroundColor = theme === Theme.Dark ? "#282c34" : "#ffffff"; // mimics CodeMirror
 
   return (
     <>
       <div
-        ref={diagramWrapperRef}
         style={{
           filter: isStale ? "blur(4px)" : undefined,
           backgroundColor,
@@ -43,13 +49,19 @@ const Diagram = ({ theme = Theme.Light, isStale, mermaidCode }: Props) => {
       >
         <Mermaid chart={mermaidCode} config={{ mermaid: { theme } }} />
       </div>
-      <div className={"mt-3"}>
+      <div className={"mt-3 d-flex justify-content-between"}>
         {/* TODO: Downloaded diagram always matches website's theme. Consider always downloading the "light" one. */}
         <Button
-          onClick={onClickSave}
+          onClick={onClickDownloadSvg}
           disabled={isStale || mermaidCode.trim().length === 0}
         >
           Download SVG
+        </Button>
+        <Button
+          onClick={onClickCopyMermaidCode}
+          disabled={isStale || mermaidCode.trim().length === 0}
+        >
+          Copy Mermaid
         </Button>
       </div>
     </>
