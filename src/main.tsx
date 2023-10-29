059e8537e1824c8fa175b219745a0f9a144bf795
eecavanna <eecavanna@users.noreply.github.com>
2023-10-28 18:01:11 -0700

    Log the app version number to the browser console

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+// Print the app version to the browser console.
+console.info(`🚀 Monocle version:`, PACKAGE_VERSION);
+
+// Render the React app.
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    PACKAGE_VERSION: JSON.stringify(process.env.npm_package_version),
+  }
 })
