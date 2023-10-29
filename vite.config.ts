993c6e1c3fb8d55a5bb3b2b38705ec18fc51f3b5
eecavanna <eecavanna@users.noreply.github.com>
2023-10-28 18:17:55 -0700

    Install `gh-pages` package and document deployment process

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/monocle/", // reference: https://vitejs.dev/guide/static-deploy.html#github-pages
   plugins: [react()],
   define: {
     PACKAGE_VERSION: JSON.stringify(process.env.npm_package_version),
